38027b184e39858de21d439a503e018cf1d91237
himancifer <himangshushekharjha@gmail.com>
2020-05-08 01:13:49 +0530

    Implementation of Weighted Interval Scheduling

---
+++ DP/WeightedIntervalScheduling.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Interval{
+    int start;
+    int end;
+    int weight;
+    Interval(int start,int end,int weight){
+        this.start= start;
+        this.end = end;
+        this.weight= weight;
+    }
+}
+
+class FinishTimeComparator implements Comparator<Interval>{
+    @Override
+    public int compare(Interval interval1,Interval interval2){
+        return interval1.end <= interval2.end ? -1 : 1;
+    }
+}
+
+public class WeightedIntervalScheduling {
+
+    static int intervalList(Interval[] intervals){
+        Arrays.sort(intervals,new FinishTimeComparator());
+        int dp[] = new int[intervals.length];
+        
+        dp[0] = intervals[0].weight;
+
+        for(int i = 1 ; i < intervals.length; i++){
+            dp[i] = Math.max(intervals[i].weight, dp[i-1]);
+            for(int j = i-1; j >=0 ; j--){
+                if(intervals[j].end <= intervals[i].start){
+                    dp[i] = Math.max(dp[i],intervals[i].weight + dp[j]);
+                    break;
+                }
+            }
+        }
+
+        int maximumWeight = Integer.MIN_VALUE;
+        for(int weight : dp){
+            maximumWeight = Math.max(maximumWeight,weight);
+        }
+
+        return maximumWeight;
+    }
+
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the number of intervals : ");
+        int n = sc.nextInt();
+        Interval intervals[] = new Interval[n];
+
+        for(int inputs = 0 ; inputs < n; inputs++){
+            System.out.println("Enter the the triple : (startInterval,endInterval,weight) for interval "+ (inputs+1) + " :: ");
+            int start = sc.nextInt(); 
+            int end = sc.nextInt(); 
+            int weight = sc.nextInt();
+            intervals[inputs] = new Interval(start, end, weight); 
+        }
+
+        System.out.println("Maximum weight : "+ intervalList(intervals));
+        
+    }
+    
+}
